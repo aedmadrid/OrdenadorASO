058f84d019bf63a7c22afb208239ad2e8d5fb65e
lucasengithub <86058446+lucasengithub@users.noreply.github.com>
2025-12-30 18:18:03 +0100

    Disable mutable users and update KWin shortcuts

--- configuration.nix
+++ configuration.nix
@@ -120,8 +120,11 @@ in
 
   };
 
+  # ============================================
+  # USUARIOS: Definir
+  # ============================================
 
-  # Define a user account sin contraseña
+  users.mutableUsers = false;
   users.users.aso = {
     isNormalUser = true;
     description = "Asociación de Estudiantes de Diseño de Madrid";
@@ -237,8 +240,11 @@ in
 
       # Atajos de teclado personalizados
       shortcuts = {
-        "kwin"."Window Close" = "Alt+F4";
-        "kwin"."Window Fullscreen" = "Meta+F11";
+        "kwin"."Window Close" = "Ctrl+W";
+        "kwin"."Window Minimize" = "Ctrl+H";
+        "kwin"."Window Maximize" = "Ctrl+M";
+        "kwin"."Window Restore" = "Ctrl+M";
+        "kwin"."Window Fullscreen" = "Ctrl+Shift+F11";
       };
     };
 
@@ -470,31 +476,6 @@ in
     };
   };
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It's perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.11"; # Did you read the comment?
 
 }
